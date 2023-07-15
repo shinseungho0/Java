03f95b7ef90b0f9157c53542d6f1c22831dfb2ad
shinseungho0 <100738567+shinseungho0@users.noreply.github.com>
2023-07-16 01:08:44 +0900

    Create String_escape.java

---
+++ base_study/String_escape.java
@@ -0,0 +1,26 @@
+package sec03.exam02;
+
+public class RunStatementExample {
+	public static void main(String[] args) {
+		/* 
+		 * 이스케이프(escape): 문자열 내부에 역슬래시(\)가 붙은 문자를 사용가능
+		 * 이스케이프 문자를 사용하면 문자열 내부에 특정 문자를 포함가능
+		 */
+		String a = "지금은 \"여름\" "; // 큰따옴표사용 \"
+		String b = "번호\t이름\t나이"; // 탭만큰 띄위기 \t
+		String c = "1\n신\n25"; // 한 줄 내림 \n
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
+
+/* 출력
+
+지금은 "여름" 
+번호	이름	나이
+1
+신
+25
+*/  
